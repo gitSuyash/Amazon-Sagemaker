b108417641998e7c8c3fe6f74899810e6597c895
Suyash <suyag10+git@gmail.com>
2020-07-19 19:28:35 +0530

    update scripts

--- sklearn/.ipynb_checkpoints/sklearn-hyperparameter-tune-checkpoint.py
+++ sklearn/.ipynb_checkpoints/sklearn-hyperparameter-tune-checkpoint.py
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     parser.add_argument('--max_depth',type=int,default=5)
 
     # Sagemaker specific arguments. Defaults are set in the environment variables.
-    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+    # parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+    # parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
 
     args = parser.parse_args()
@@ -35,8 +35,7 @@ if __name__ == '__main__':
     train_y = train_data.iloc[:,0]
     train_X = train_data.iloc[:,1:]
 
-    # Here we support a single hyperparameter, 'max_leaf_nodes'. Note that you can add as many
-    # as your training my require in the ArgumentParser above.
+    #seting hyperparameters
     max_leaf_nodes = args.max_leaf_nodes
     max_depth = args.max_depth
 
@@ -53,12 +52,3 @@ if __name__ == '__main__':
 
     # Print the coefficients of the trained classifier, and save the coefficients
     joblib.dump(tuner, os.path.join(args.model_dir, "model.joblib"))
-
-
-def model_fn(model_dir):
-    """Deserialized and return fitted model
-
-    Note that this should have the same name as the serialized model in the main method
-    """
-    final_clf = joblib.load(os.path.join(model_dir, "model.joblib"))
-    return final_clf
--- sklearn/scikit-script.py
+++ sklearn/scikit-script.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     parser.add_argument('--max_depth',type=int,default=5)
 
     # Sagemaker specific arguments. Defaults are set in the environment variables.
-    parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
-    parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
+#     parser.add_argument('--output-data-dir', type=str, default=os.environ['SM_OUTPUT_DATA_DIR'])
+#     parser.add_argument('--model-dir', type=str, default=os.environ['SM_MODEL_DIR'])
     parser.add_argument('--train', type=str, default=os.environ['SM_CHANNEL_TRAIN'])
 
     args = parser.parse_args()
@@ -47,10 +47,10 @@ if __name__ == '__main__':
     joblib.dump(clf, os.path.join(args.model_dir, "model.joblib"))
 
 
-def model_fn(model_dir):
-    """Deserialized and return fitted model
+# def model_fn(model_dir):
+#     """Deserialized and return fitted model
 
-    Note that this should have the same name as the serialized model in the main method
-    """
-    clf = joblib.load(os.path.join(model_dir, "model.joblib"))
-    return clf
+#     Note that this should have the same name as the serialized model in the main method
+#     """
+#     clf = joblib.load(os.path.join(model_dir, "model.joblib"))
+#     return clf
